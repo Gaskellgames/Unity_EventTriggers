9ae5a342323cd50dc0e2c120ad8ce4111bb60840
Gaskellgames <77272073+Gaskellgames@users.noreply.github.com>
2023-06-15 10:38:32 +0100

    Initia Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+/// Summary
+
+Code created by Gaskellgames. Available to use under MIT Licence
+
+
+/// Description
+  
+Two simple C# scripts used to trigger events from Unity's inspector.
+
+The script allows users to trigger events from either a trigger target object, or from a trigger sensor object.
+Contains 2 scripts: TriggerActionSensor & TriggerActionTarget.
+
+
+/// Important
+
+The scripts require use of the Gaskellgames shared library scripts for custom inspector properties: Inspector Events & Readonly Inspector Fields.
---
+++ Unity_EventTriggers/TriggerActionTarget.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+using Gaskellgames;
+
+/// <summary>
+/// Code created by Gaskellgames
+/// </summary>
+
+public class TriggerActionTarget : MonoBehaviour
+{
+    #region Variables
+
+    [Space][TagDropdown, SerializeField] private string triggerTag = "";
+    [Space][SerializeField] private InspectorEvent OnEnterActionSensor;
+    [Space][SerializeField] private InspectorEvent OnExitActionSensor;
+
+    #endregion
+
+    //----------------------------------------------------------------------------------------------------
+
+    #region Triggers
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(triggerTag))
+        {
+            OnEnterActionSensor.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(triggerTag))
+        {
+            OnExitActionSensor.Invoke();
+        }
+    }
+
+    #endregion
+
+    //----------------------------------------------------------------------------------------------------
+
+    #region Public Functions
+
+    public void ForceExit()
+    {
+        OnExitActionSensor.Invoke();
+    }
+
+    #endregion
+
+} //class end
